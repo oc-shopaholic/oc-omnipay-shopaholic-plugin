eb74c22ac484764c5295df45daf763114f008a43
Sebastiaan Kloos <sebastiaan@codecycler.com>
2021-04-14 19:24:08 +0200

    Added web group middleware to payment return urls

--- routes.php
+++ routes.php
@@ -5,9 +5,9 @@ use Lovata\OmnipayShopaholic\Classes\Helper\PaymentGateway;
 Route::get(PaymentGateway::SUCCESS_RETURN_URL.'{slug}', function ($sSecretKey) {
     $obPaymentGateway = new PaymentGateway();
     return $obPaymentGateway->processSuccessRequest($sSecretKey);
-});
+})->middleware('web');
 
 Route::get(PaymentGateway::CANCEL_RETURN_URL.'{slug}', function ($sSecretKey) {
     $obPaymentGateway = new PaymentGateway();
     return $obPaymentGateway->processCancelRequest($sSecretKey);
-});
+})->middleware('web');
